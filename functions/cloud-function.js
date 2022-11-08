5e3851d0fb44faf4c5fc049b12be39621d76f4dd
Anuj Pundalik <72062428+anujcontentstack@users.noreply.github.com>
2022-11-08 14:41:03 +0530

    contentfly-management-background-jobs-b

--- functions/cloud-function.js
+++ functions/cloud-function.js
@@ -5,5 +5,5 @@ export default function handler(_, res) {
 
   res
     .status(200)
-    .json({ deploymentIdentifier: contentfly-management-background-jobs-a })
+    .json({ deploymentIdentifier: contentfly-management-background-jobs-b })
 }
