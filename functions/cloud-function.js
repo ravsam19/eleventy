0d8ed7b90dd71693651cde5b3b8f40aaf5203ea2
Anuj Pundalik <72062428+anujcontentstack@users.noreply.github.com>
2022-11-08 13:15:41 +0530

    random-11

--- functions/cloud-function.js
+++ functions/cloud-function.js
@@ -5,5 +5,5 @@ export default function handler(_, res) {
 
   res
     .status(200)
-    .json({ deploymentIdentifier: multiple-files-commit-working })
+    .json({ deploymentIdentifier: random-11 })
 }
