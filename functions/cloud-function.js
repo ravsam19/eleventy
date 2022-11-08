0d4a535c9ca84e47f7a02e7835b3fcc02a894277
Anuj Pundalik <72062428+anujcontentstack@users.noreply.github.com>
2022-11-08 13:07:42 +0530

    multiple-files-commit-updated

--- functions/cloud-function.js
+++ functions/cloud-function.js
@@ -1,9 +1,9 @@
-export default function handler(req, res) {
-  if (process.env.envVariable !== "something") {
-    throw new Error("Something");
+export default function handler(_, res) {
+  if (process.env.TEST_VARIABLE !== "TEST_VALUE") {
+    throw new Error("Env variable not present");
   }
 
   res
     .status(200)
-    .json({ deploymentIdentifier: req.query.deploymentIdentifier })
+    .json({ multiple-files-commit-updated })
 }
