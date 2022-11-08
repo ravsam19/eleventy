e5af8a5613620bfa1e80901cb6ea8e6324bd3561
Anuj Pundalik <anuj.pundalik@contentstack.com>
2022-11-08 10:31:19 +0530

    Trying env var

--- functions/cloud-function.js
+++ functions/cloud-function.js
@@ -1,4 +1,8 @@
 export default function handler(req, res) {
+  if (process.env.envVariable !== "something") {
+    throw new Error("Something");
+  }
+
   res
     .status(200)
     .json({ deploymentIdentifier: req.query.deploymentIdentifier })
