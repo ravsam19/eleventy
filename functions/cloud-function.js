f9a8d35cd3cc49da4670002082f2c487e1da9124
Anuj Pundalik <72062428+anujcontentstack@users.noreply.github.com>
2022-11-09 11:09:48 +0530

    random-working-commit-l6

--- functions/cloud-function.js
+++ functions/cloud-function.js
@@ -5,5 +5,5 @@ export default function handler(_, res) {
 
   res
     .status(200)
-    .json({ deploymentIdentifier: random-working-commit-l5 })
+    .json({ deploymentIdentifier: random-working-commit-l6 })
 }
