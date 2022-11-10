5490d8b53c789c46ae1fabd1780f87faafc24a2c
Anuj Pundalik <72062428+anujcontentstack@users.noreply.github.com>
2022-11-10 16:33:14 +0530

    somerandom:commit-random-id

--- functions/cloud-function.js
+++ functions/cloud-function.js
@@ -5,5 +5,5 @@ export default function handler(_, res) {
 
   res
     .status(200)
-    .json({ deploymentIdentifier: "somerandom:commit-random" })
+    .json({ deploymentIdentifier: "somerandom:commit-random-id" })
 }
