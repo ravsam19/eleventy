24856ca255d585d97cc6b191986af77042053b9a
Anuj Pundalik <72062428+anujcontentstack@users.noreply.github.com>
2022-11-10 15:00:35 +0530

    somerandom:commit

--- functions/cloud-function.js
+++ functions/cloud-function.js
@@ -5,5 +5,5 @@ export default function handler(_, res) {
 
   res
     .status(200)
-    .json({ deploymentIdentifier: "nextjs-commit-working-32" })
+    .json({ deploymentIdentifier: "somerandom:commit" })
 }
