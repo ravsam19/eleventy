0eca1f84a25667bc9ebac2a8e3f8ad9e5ff64504
Anuj Pundalik <72062428+anujcontentstack@users.noreply.github.com>
2022-11-10 22:58:32 +0530

    random-working-commit-23

--- functions/cloud-function.js
+++ functions/cloud-function.js
@@ -5,5 +5,5 @@ export default function handler(_, res) {
 
   res
     .status(200)
-    .json({ deploymentIdentifier: "random-working-commit-l3" })
+    .json({ deploymentIdentifier: "random-working-commit-23" })
 }
